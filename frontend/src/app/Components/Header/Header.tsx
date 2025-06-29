d73c5236792191c8889da234464d3f772fc6d446
flaviomelian <flaviomelian2000@gmail.com>
2025-06-29 01:58:01 +0100

    header estilado, no navega

--- frontend/src/app/Components/Header/Header.tsx
+++ frontend/src/app/Components/Header/Header.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import Image from "next/image";
+import "./Header.css";
+import home from "../../assets/home.png";
+import audioFile from "../../assets/audio_file.png";
+import mic from "../../assets/mic.png";
+import account from "../../assets/account.png";
 
 const Header = () => {
   return (
-    <div className='header'>
-      HEADER
+    <div className="header">
+      <Image className="in-header" src={home} alt="Home" />
+      <div className="options">
+        <Image className="in-header" src={audioFile} alt="Audio" />
+        <Image className="in-header" src={mic} alt="Mic" />
+        <Image className="in-header" src={account} alt="Account" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,6 +8,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double:slnt,wght@-8,235&display=swap"
+          rel="stylesheet"/>
+      </head>
       <body>
         <Header />
         {children} {/* Aquí Next.js pone el contenido de cada página */}
